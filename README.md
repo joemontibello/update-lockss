fdbc8a76bc96ccb2d614d2e64098203f48ff3dd1
Joe M <joseph.montibello@dartmouth.edu>
2014-06-06 13:47:39 -0400

    Added spacing, fixing style issues that PyCharm found.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 locksstracking
 
-I was interested in how long it would take my local LOCKSS node to collect all the Archival Units that my institution has selected.
+I was interested in how long it would take my local LOCKSS node to collect all the Archival Units that my institution
+has selected.
 
 My google docs spreadsheet has a bunch of formulas in it to calculate an average time to successfully collect the 
 Archival Units that have been selected for collection.
@@ -9,8 +10,9 @@ This python script looks up the appropriate numbers on the lockss node's website
 them into the spreadsheet. I've been running the script daily via a cron job on my local machine.
 
 Requirements:
-I'm using Python 2.7 on a mac (and haven't tested this in any other environment). The script requires the 'requests' python module and a few other pieces, listed in the 
-early part of the script. The usernames and passwords required are in a separate text file, credgapi.
+I'm using Python 2.7 on a mac (and haven't tested this in any other environment). The script requires the 'requests'
+python module and a few other pieces, listed in the early part of the script. The usernames and passwords required are
+in a separate text file, credgapi.
 
 TO-DO:
 
@@ -18,10 +20,11 @@ Find a way to update the formula cells in the spreadsheet. Right now I let the s
 I manually open the document once in a while and use command-D to fill the formulas down from the last filled in line.
 It would be better to do this each time the sheet is updated with new numbers.
 
-Better integrate passwords so that they're not kept in plaintext in a file - maybe encrypt them?
+Replace password authentication with something else - OAuth2?
 
-Build a set-up module that would ask the user for the relevant info - usernames, passwords, url's, when to run the script, etc.
-The set-up module would then create the passwords text file, url's, etc, and install a cron job.
+Build a set-up module that would ask the user for the relevant info - usernames, passwords, url's, when to run the
+script, etc. The set-up module would then create the passwords text file, urls, etc, and install a cron job.
 
-This software is available for anyone to use, copy, modify, and/or redistribute at will. Attribution would be appreciated but is not required.
+This software is available for anyone to use, copy, modify, and/or redistribute at will. Attribution would be
+appreciated but is not required.
 
--- lockssupdatestats.py
+++ lockssupdatestats.py
@@ -4,18 +4,18 @@
 
 #import needed modules. re, time and sys are part of python, so you
 #get them with an install of the language.
+# gdata.spreadsheet.service uses an API to read and edit google documents.
+#requests makes http requesting easier.
+
 import re
 import time
 from sys import exit
-#these two modules need to be installed in addition to python.
-#gdata.spreadsheet.service uses an API to read and edit google documents.
+
 import gdata.spreadsheet.service
-#requests makes http requesting easier.
 import requests
 
-#doc_name is used to find the right spreadsheet inside the user's google drive acct
-doc_name  = 'lockss crawl stats'
 
+#doc_name is used to find the right spreadsheet inside the user's google drive acct
 #credfile is the full path to the file that holds usernames and passwords
 #To work, you need a plain text file. It should have 4 values, each on its own
 #line, with no blank lines.  They need to be ordered like this:
@@ -25,13 +25,17 @@ doc_name  = 'lockss crawl stats'
 #  locksspassword
 #I've included credgapi as a placeholder file in git, but the name of the file
 #doesn't matter as long as the variable points to it correctly.
-credfile = '/path/to/file/credgapi'
-#creds opens the file that holds usernames and passwords.
+
+credfile = '../credgapi'
+doc_name = 'lockss crawl stats'
+
 try:
     creds = open(credfile, 'r')
+
 except IOError:
     print "Failed to open credential store at " + credfile + "\n"
     raise
+
 else:
     #If we opened the credentials properly, we can pull
     #each line into a variable, removing the "return" at the end of each.
@@ -41,42 +45,50 @@ else:
     locksspassword = creds.readline().rstrip()
     creds.close()
 
-    #next, check that we have something in each variable.
+# next, check that we have something in each variable.
+
 if email == "" or password == "" or lockssname == "" or locksspassword == "":
     print "Failed to read one or more of the needed credentials from " + credfile + "\n"
-    
+
 #now is a variable that gets the time on the computer where the script is running.
 #The LOCKSS calculations in the spreadsheet will be messed up if
-#this script runs in different time zone from the original, and 
+#this script runs in a different time zone from the original, and
 #standard time vs. daylight savings time should be handled here but it isn't.
 #It's not going to throw the numbers off enough to make me want to fix it.
-thistime=time.time()
-now=time.localtime(thistime)
+
+thistime = time.time()
+now = time.localtime(thistime)
 
 #top_level_url is the lockss server page where the latest numbers come from
-top_level_url = "http://lockss.dartmouth.edu:8081/DaemonStatus"
+#total, notcollected and needrecrawl are the numbers we need.
 
-#this chunk of code gets the page at top_level_url.
+top_level_url = "http://lockss2.dartmouth.edu:8081/DaemonStatus"
+total = ""
+notcollected = ""
+needrecrawl = " "
+
 r = requests.get(top_level_url, auth=(lockssname, locksspassword))
 #print r.text #debugging
 #requestworked = re.match(".*Archival.*", r.text) #debugging
-requestworked='<title>LOCKSS: Overview - Daemon Status</title>' in r.text
-if not requestworked :
+requestworked = '<title>LOCKSS: Overview - Daemon Status</title>' in r.text
+if not requestworked:
     print "request failed. got: \n " + r.text
     exit()
-    
-else :
+
+else:
     #We have the page, and now we use a regular expression to find
     #the numbers on that page to plug into our spreadsheet.
-    pattern=re.compile('.*ArchivalUnitStatusTable\">(.*)Archival\sUnits\s\([\d]+\sinternal\),\s(.*)\snot\scollected,\s(.*)\sneed.*', re.MULTILINE|re.DOTALL)
-    result=pattern.match(r.text)
+    pattern = re.compile(
+        '.*ArchivalUnitStatusTable\">(.*)Archival\sUnits\s\([\d]+\sinternal\),\s(.*)\snot\scollected,\s(.*)\sneed.*',
+        re.MULTILINE | re.DOTALL)
+    result = pattern.match(r.text)
     #print "result = " + result #debugging
-    if result :
-        total=result.group(1)
-        notcollected=result.group(2)
-        needrecrawl=result.group(3)
+    if result:
+        total = result.group(1)
+        notcollected = result.group(2)
+        needrecrawl = result.group(3)
     else:
-        print "Failed - " + top_level_url +" - " + r.text + "\n"
+        print "Failed - " + top_level_url + " - " + r.text + "\n"
         exit()
 
 #get logged in to Google Docs SpreadsheetsService
@@ -86,26 +98,23 @@ spr_client.password = password
 spr_client.source = 'lockss update'
 login = spr_client.ProgrammaticLogin()
 
-#get the document specified in doc_name programattically
+#get the document specified in doc_name programmatically
 q = gdata.spreadsheet.service.DocumentQuery()
 q['title'] = doc_name
 q['title-exact'] = 'true'
 sfeed = spr_client.GetSpreadsheetsFeed(query=q)
-spreadsheet_id = sfeed.entry[0].id.text.rsplit('/',1)[1]
+spreadsheet_id = sfeed.entry[0].id.text.rsplit('/', 1)[1]
 wfeed = spr_client.GetWorksheetsFeed(spreadsheet_id)
-worksheet_id = wfeed.entry[0].id.text.rsplit('/',1)[1]
+worksheet_id = wfeed.entry[0].id.text.rsplit('/', 1)[1]
 
 # Prepare a dictionary of all the data we're going to write into the spreadsheet.
 # I had a lot of trouble with column headers until I changed them all
 # to lower-case letters only. 
-lockssdict = {}
-lockssdict['date'] =  str(now.tm_mon) + '/' + str(now.tm_mday) + '/' + str(now.tm_year)
-lockssdict['time'] = str(now.tm_hour) + ':' + str(now.tm_min)
-lockssdict['totalaus'] = total
-lockssdict['notcollected'] = notcollected
-lockssdict['needingrecrawl'] = needrecrawl
+lockssdict = {'date': str(now.tm_mon) + '/' + str(now.tm_mday) + '/' + str(now.tm_year),
+              'time': str(now.tm_hour) + ':' + str(now.tm_min), 'totalaus': total, 'notcollected': notcollected,
+              'needingrecrawl': needrecrawl}
 
 # write the data into the next available row in the spreadsheet.
 entry = spr_client.InsertRow(lockssdict, spreadsheet_id, worksheet_id)
 if not isinstance(entry, gdata.spreadsheet.SpreadsheetsList):
-  print "Insert row failed."
+    print "Insert row failed."
